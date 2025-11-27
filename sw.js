ac7be931abb71ada3adc1f5dd8cf8fe7a009edfc
harish0501 <barooriharish@gmail.com>
2025-11-27 15:53:00 -0600

    Updates

--- sw.js
+++ sw.js
@@ -1,13 +1,2 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
-
-self.addEventListener('push', event => {
-  const data = event.data?.json() || { title: 'EATFORCE', body: 'Time to dominate.' };
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: '/icon-192.png',
-    badge: '/icon-192.png',
-    vibrate: [200, 100, 200],
-    tag: 'eatforce-alert'
-  });
-});
